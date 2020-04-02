7ec6c351c6b24e9605523049ec8bab7e7fe12ef6
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-02 12:33:50 +0100

    Added the world stats component to show the total cases, deaths and recoveries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Worldcases from './components/Worldcases.js'
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [latest, setLatest] = React.useState('');
+
+  useEffect(() =>{
+    axios.get('https://corona.lmao.ninja/all').then(response => setLatest(response.data))
+  }, [])
+
+  const lastUpdated = new Date(parseInt(latest.updated)).toUTCString()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COVID-199999999999999999999999999</h1>
+      <Worldcases cases = {latest.cases} recoveries = {latest.recovered} deaths = {latest.deaths} updated = {lastUpdated} />
     </div>
   );
 }
