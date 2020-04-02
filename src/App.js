03c9ed7369da775ff8d9cc04a778456f60b5c124
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-02 16:23:59 +0100

    Added the onclick functionality for the search to only occur when the search button is clicked

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import Countries from './components/Countries.js';
 function App() {
   const [latest, setLatest] = React.useState([]);
   const [results, setResults] = React.useState([]);
-  const [searchCountry, setSearchCountry] = React.useState('')
+  const [searchCountry, setSearchCountry] = React.useState('');
+  const [typedCountry, setTypedCountry] = React.useState('')
 
   const handleSearch = (e) =>{
-    setSearchCountry(e.target.value)
+    setTypedCountry(e.target.value)
   }
 
   useEffect(() =>{
@@ -28,8 +29,12 @@ function App() {
 
   const lastUpdated = new Date(parseInt(latest.updated)).toUTCString();
 
+  const typedToSearch = () => {
+    setSearchCountry(typedCountry)
+  };
+
   const filterCountry = results.filter(item => {
-    return searchCountry !== '' ? item.country.toLowerCase() === searchCountry.toLowerCase() : item
+    return searchCountry !== '' ? item.country.toLowerCase().includes(searchCountry.toLowerCase()) : item
   });
 
   const countries = filterCountry.map(result => {
@@ -52,7 +57,7 @@ function App() {
     <div className="App">
       <h1>COVID-19 Live Tracker</h1>
       <Worldcases cases = {latest.cases} recoveries = {latest.recovered} deaths = {latest.deaths} updated = {lastUpdated} />
-      <Search onChange = {handleSearch} value = {searchCountry} onClick = {filterCountry} />
+      <Search onChange = {handleSearch} value = {typedCountry} onClick = {typedToSearch} />
       <div className= 'worldstats'>{countries}</div>
     </div>
   );
