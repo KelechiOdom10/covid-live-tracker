24324e56766192b0da030f05ac4360fd55da8340
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-02 14:17:14 +0100

    Added a map to show the results for the different countries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import Worldcases from './components/Worldcases.js'
+import Worldcases from './components/Worldcases.js';
+import {Card} from 'react-bootstrap';
 import axios from 'axios';
 import './App.css';
 import Search from './components/Search.js';
@@ -27,11 +28,30 @@ function App() {
 
   const lastUpdated = new Date(parseInt(latest.updated)).toUTCString()
 
+  const countries = results.map(result => {
+    return(
+      <div className = 'box'>
+        <Card style={{ width: '20rem' }}>
+          <Card.Img variant="top" src={result.countryInfo.flag} style ={{height: '180px'}} />
+          <Card.Body>
+            <Card.Title style = {{fontWeight: 'bold', fontSize: '28px'}}>{result.country}</Card.Title>
+            <Card.Text style = {{borderBottom: '0.02px solid grey'}}><span style ={{fontWeight: 'bold'}}>Cases: </span>{result.cases}</Card.Text>
+            <Card.Text style = {{borderBottom: '0.02px solid grey'}}><span style ={{fontWeight: 'bold'}}>Recovered: </span>{result.recovered}</Card.Text>
+            <Card.Text style = {{borderBottom: '0.02px solid grey'}}><span style ={{fontWeight: 'bold'}}>Deaths: </span>{result.deaths}</Card.Text>
+            <Card.Text style = {{borderBottom: '0.02px solid grey'}}><span style ={{fontWeight: 'bold'}}>Active: </span>{result.active}</Card.Text>
+            <Card.Text style = {{borderBottom: '0 solid grey'}}><span style ={{fontWeight: 'bold'}}>Critical: </span>{result.critical}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    )
+  })
+
   return (
     <div className="App">
-      <h1>COVID-199999999999999999999999999</h1>
+      <h1>COVID-19 Live Tracker</h1>
       <Worldcases cases = {latest.cases} recoveries = {latest.recovered} deaths = {latest.deaths} updated = {lastUpdated} />
       <Search onChange = {handleSearch}  />
+      <div className= 'worldstats'>{countries}</div>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,15 +3,17 @@ import {InputGroup, FormControl, Button} from 'react-bootstrap';
 
 function Search(props) {
     return (
-    <InputGroup className="mb-3" style = {{maxWidth: '800px', margin: '0 auto'}}>
-        <FormControl onChange = {props.onChange}
-            placeholder="Search country..."
-        />
-        <InputGroup.Append>
-            <Button onClick = {props.onClick} variant="outline-secondary">Search</Button>
-        </InputGroup.Append>
-    </InputGroup>
-  )
+        <div className = 'search'>
+            <InputGroup className="mb-3" style = {{maxWidth: '800px', margin: '0 auto'}}>
+                <FormControl onChange = {props.onChange}
+                    placeholder="Search country..."
+                />
+                <InputGroup.Append>
+                    <Button onClick = {props.onClick} variant="outline-secondary">Search</Button>
+                </InputGroup.Append>
+            </InputGroup>
+        </div>
+    )
 }
 
 export default Search
