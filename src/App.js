6ae951815fab399dcb1a8d2db20ae685f2627990
KelechiOdom10 <kelechiodom10@gmail.com>
2020-04-07 17:08:18 +0100

    Added the NavBar functionality which works and links to the About and Contact pages. This also contains a logo with the right styling and a change in the body background

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Router, Link} from '@reach/router'
 import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Contact from './components/Contact'
+import About from './components/About'
+
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Router>
         <Home path='/' />
+        <Contact path = 'contact' />
+        <About path = 'about' />
       </Router>   
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from '@reach/router'
+
+function NavBar() {
+    return(
+        <div className = 'nav'>
+            <Link 
+                to = '/' 
+                style = {{color:'white'}}>
+                <img src = './assets/covid.svg'  style = {{height: '6vh'}}/>
+                <span className = 'covididentifier'>COVID-19 Live Tracker</span></Link>
+            <ul className = 'nav-links'>
+                <Link to ='/' style = {{color: 'white'}}><li>Home</li></Link>
+                <Link to ='about' style = {{color: 'white'}}><li className = 'li-1'>About</li></Link>
+                <Link to ='contact' style = {{color: 'white'}}><li>Contact</li></Link>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
